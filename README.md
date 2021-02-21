62ec4ac69b521f936f9b16bec2de6f48879847f4
K.Sielski <kris@sielski.com>
2021-02-21 17:25:40 -0500

    Updated to 2021 Verbage

---
+++ README.md
@@ -0,0 +1,8 @@
+# IR@Home 2021 
+Code for FRC 2021 game Infinite Recharge at Home
+
+Cloned from Infinite Recharge 2020 as the base
+
+Currently compiled under 2020 Environment (version 2020.3.2)
+
+
--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -46,7 +46,8 @@ void Write2Dashboard(void);
 void Robot::RobotInit() {
     //*************************** INIT ******************************
     std::cout<<"RobotInit"<<std::endl;
-    std::cout<<"FRC2020: Infinite Recharge ~~~DotMatrix~~~"<<std::endl;
+    std::cout<<"FRC2021: Infinite Recharge@Home"<<std::endl;
+    std::cout<<"       ~~~DotMatrix~~~"<<std::endl;
     std::cout<<"Version: " << __DATE__ <<"  "<<__TIME__<<std::endl<<std::endl; 
 
 
